88f5bd15fb3f15173b58a2242094082a1f6fd4e9
MalCaor <xav.lemen@orange.fr>
2022-07-05 19:36:40 +0200

    text script get comp

--- Script/CameraCanva/TimerConter.cs
+++ Script/CameraCanva/TimerConter.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TimerConter : MonoBehaviour
 {
+    // vars
+    TextMeshProUGUI text;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        text = GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
