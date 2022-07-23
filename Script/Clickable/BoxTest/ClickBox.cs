6abd57d27b1a3ef8f197d79e840f713d2f62fe78
MalCaor <xav.lemen@orange.fr>
2022-07-23 18:58:39 +0200

    inspec window

--- Script/Clickable/BoxTest/ClickBox.cs
+++ Script/Clickable/BoxTest/ClickBox.cs
@@ -5,15 +5,18 @@ using TMPro;
 
 public class ClickBox : MonoBehaviour
 {
+    GameObject InspecWindow;
     GameObject Libel;
     void Start()
     {
+        InspecWindow = GameObject.Find("Main Camera/Canvas Camera/DecriptionWindow");
         Libel = GameObject.Find("Main Camera/Canvas Camera/DecriptionWindow/Libel");
     }
 
     // when the mouse click the object
     void OnMouseDown()
     {
+        InspecWindow.SetActive(true);
         Libel.GetComponent<TextMeshProUGUI>().text = this.name;
     }
 }
