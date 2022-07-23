00397007053fcade78994a2d06b71ad1a0daf37f
MalCaor <xav.lemen@orange.fr>
2022-07-23 18:34:02 +0200

    descript window

---
+++ Script/Clickable/BoxTest/ClickBox.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ClickBox : MonoBehaviour
+{
+    GameObject Libel;
+    void Start()
+    {
+        Libel = GameObject.Find("Main Camera/Canvas Camera/DecriptionWindow/Libel");
+    }
+
+    // when the mouse click the object
+    void OnMouseDown()
+    {
+        Libel.GetComponent<TextMeshProUGUI>().text = "cube";
+    }
+}
