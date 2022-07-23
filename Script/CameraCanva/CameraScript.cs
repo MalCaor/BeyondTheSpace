143234388bf5e5f3f2d429a514ca5d048aaba45b
MalCaor <xav.lemen@orange.fr>
2022-07-23 16:05:28 +0200

    mouvement cam rotation

--- Script/CameraCanva/CameraScript.cs
+++ Script/CameraCanva/CameraScript.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     //vars
-    float speed = 1f;
+    // speed of mouvement
+    public float speed = 1f;
+    // sensivity of camera rotation
+    public float sensitivity = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -22,21 +25,33 @@ public class CameraScript : MonoBehaviour
     // mouve cam update
     void UpdateCam()
     {
+        // arrow key (relative to camera not the world)
         if(Input.GetKey(KeyCode.RightArrow))
         {
+            // go right
             transform.Translate(new Vector3(speed,0,0));
         }
         if(Input.GetKey(KeyCode.LeftArrow))
         {
+            // go Left
             transform.Translate(new Vector3(-speed,0,0));
         }
         if(Input.GetKey(KeyCode.DownArrow))
         {
-            transform.Translate(new Vector3(0,-speed,0));
+            // go Back
+            transform.Translate(new Vector3(0,0,-speed));
         }
         if(Input.GetKey(KeyCode.UpArrow))
         {
-            transform.Translate(new Vector3(0,speed,0));
+            // go Front
+            transform.Translate(new Vector3(0,0,speed));
+        }
+
+        // rotation
+        if(Input.GetMouseButton(2))
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X")* sensitivity, 0);
+            transform.Rotate(-Input.GetAxis("Mouse Y")* sensitivity, 0, 0);
         }
     }
 }
