342396f297f30d388d60c09d87a5cf7283b96789
MalCaor <xav.lemen@orange.fr>
2022-07-23 16:12:38 +0200

    ZQSD

--- Script/CameraCanva/CameraScript.cs
+++ Script/CameraCanva/CameraScript.cs
@@ -26,25 +26,35 @@ public class CameraScript : MonoBehaviour
     void UpdateCam()
     {
         // arrow key (relative to camera not the world)
-        if(Input.GetKey(KeyCode.RightArrow))
+        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             // go right
             transform.Translate(new Vector3(speed,0,0));
         }
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.Q))
         {
             // go Left
             transform.Translate(new Vector3(-speed,0,0));
         }
-        if(Input.GetKey(KeyCode.DownArrow))
+        if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
             // go Back
             transform.Translate(new Vector3(0,0,-speed));
         }
-        if(Input.GetKey(KeyCode.UpArrow))
+        if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Z))
         {
             // go Front
             transform.Translate(new Vector3(0,0,speed));
+        }
+        if(Input.GetKey(KeyCode.Space))
+        {
+            // go Up
+            transform.Translate(new Vector3(0,speed,0));
+        }
+        if(Input.GetKey(KeyCode.C))
+        {
+            // go Down
+            transform.Translate(new Vector3(0,-speed,0));
         }
 
         // rotation
