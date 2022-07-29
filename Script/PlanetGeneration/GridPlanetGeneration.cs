f9c987049b71cac1c5b1d1aa2e5b6eb5b4566028
MalCaor <xav.lemen@orange.fr>
2022-07-29 22:39:29 +0200

    grid gener

--- Script/PlanetGeneration/GridPlanetGeneration.cs
+++ Script/PlanetGeneration/GridPlanetGeneration.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class GridPlanetGeneration : MonoBehaviour
 {
+    // vars Pub
+    // rayon
+    public float rayonVoid;
+    public float rayonOrbite;
+    public float resolution = 1f;
+
+    // vars Priv
+    // List Points grid
+    List<Vector3> listPointGrid;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Generate grid
+        generateGrid();
     }
 
-    // Update is called once per frame
-    void Update()
+    void generateGrid()
     {
-        
+        // init
+        listPointGrid = new List<Vector3>();
+        // parcour
+        for (float x = transform.localPosition.x - rayonOrbite; x < transform.localPosition.x + rayonOrbite; x+=resolution)
+        {
+            for (float y = transform.localPosition.y - rayonOrbite; y < transform.localPosition.y + rayonOrbite; y+=resolution)
+            {
+                for (float z = transform.localPosition.z - rayonOrbite; z < transform.localPosition.z + rayonOrbite; z+=resolution)
+                {
+                    // add the point to the list
+                    listPointGrid.Add(new Vector3(x, y, z));
+                }
+            }
+        }
     }
 }
