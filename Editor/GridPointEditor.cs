9b76a001055443bd16d995b4d528067aba35e3f2
MalCaor <xav.lemen@orange.fr>
2022-08-05 17:17:24 +0200

    better editor proxy point

--- Editor/GridPointEditor.cs
+++ Editor/GridPointEditor.cs
@@ -18,23 +18,19 @@ public class GridPointEditor : Editor
             EditorGUI.indentLevel++;
             for (int x = 0; x < point.matricePoint.GetLength(0); x++)
             {
-                bool showGridPointX = EditorGUILayout.Foldout(showGridPoint, "Grid Axe X : (" + x +")");
-                if(showGridPointX) {
+                EditorGUILayout.LabelField("X : " + (x-1));
+                EditorGUI.indentLevel++;
+                for (int z = 0; z < point.matricePoint.GetLength(1); z++)
+                {
+                    EditorGUILayout.LabelField("Z : " + (z-1));
                     EditorGUI.indentLevel++;
-                    for (int z = 0; z < point.matricePoint.GetLength(1); z++)
+                    for (int y = 0; y < point.matricePoint.GetLength(2); y++)
                     {
-                        bool showGridPointZ = EditorGUILayout.Foldout(showGridPoint, "Grid Axe Z : (" + z +")");
-                        if(showGridPointZ) {
-                            EditorGUI.indentLevel++;
-                            for (int y = 0; y < point.matricePoint.GetLength(2); y++)
-                            {
-                                GameObject p = (GameObject)EditorGUILayout.ObjectField("Point : " + (x-1) + ", " + (z-1) + ", " + (y-1), point.matricePoint[x,z,y], typeof(GameObject), true);
-                            }
-                            EditorGUI.indentLevel--;
-                        }
+                        GameObject p = (GameObject)EditorGUILayout.ObjectField("Point : " + (x-1) + ", " + (z-1) + ", " + (y-1), point.matricePoint[x,z,y], typeof(GameObject), true);
                     }
                     EditorGUI.indentLevel--;
                 }
+                EditorGUI.indentLevel--;
             }
             EditorGUI.indentLevel--;
         }
