b8cdc1338ec95f4602e7429d5e785441a1ac130f
MalCaor <xav.lemen@orange.fr>
2022-08-06 10:50:02 +0200

    save matrice proxy

--- Script/PlanetGeneration/SubClass/GridPointProxyMatrice.cs
+++ Script/PlanetGeneration/SubClass/GridPointProxyMatrice.cs
@@ -3,8 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class GridPointProxyMatrice
+public class GridPointProxyMatrice : ISerializationCallbackReceiver
 {
     [SerializeField]
     public GameObject[,,] matricePoint = new GameObject[3,3,3];
+
+    // serialisation
+    [SerializeField]
+    List<GameObject> saveMatricePoint;
+    [SerializeField]
+    int xSave;
+    [SerializeField]
+    int zSave;
+    [SerializeField]
+    int ySave;
+    public void OnBeforeSerialize()
+    {
+        saveMatricePoint = new List<GameObject>();
+        xSave = matricePoint.GetLength(0);
+        zSave = matricePoint.GetLength(1);
+        ySave = matricePoint.GetLength(2);
+        for (int x = 0; x < xSave; x++)
+        {
+            for (int z = 0; z < zSave; z++)
+            {
+                for (int y = 0; y < ySave; y++)
+                {
+                    saveMatricePoint.Add(matricePoint[x,z,y]);
+                }
+            }
+        }
+    }
+    public void OnAfterDeserialize()
+    {
+        int i = 0;
+        for (int x = 0; x < xSave; x++)
+        {
+            for (int z = 0; z < zSave; z++)
+            {
+                for (int y = 0; y < ySave; y++)
+                {
+                    matricePoint[x,z,y] = saveMatricePoint[i];
+                    i++;
+                }
+            }
+        }
+    }
 }
