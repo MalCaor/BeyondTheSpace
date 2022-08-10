5ea087425bf57bee78b16c19cab4a315750a2928
MalCaor <xav.lemen@orange.fr>
2022-08-10 14:33:05 +0200

    fjhezohfbiezlkfb

---
+++ Script/PlanetGeneration/GridIcoSphereGeneration.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridIcoSphereGeneration : MonoBehaviour
+{
+    public PlanetGenerationSettings planetSettings;
+    
+}
