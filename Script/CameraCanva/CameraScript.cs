191cd66972c8defd417188802dadf1574521c5fe
MalCaor <xav.lemen@orange.fr>
2022-08-23 10:26:33 +0200

    vue du sol

--- Script/CameraCanva/CameraScript.cs
+++ Script/CameraCanva/CameraScript.cs
@@ -10,6 +10,9 @@ public class CameraScript : MonoBehaviour
     // sensivity of camera rotation
     public float sensitivity = 10f;
 
+    // curent grid to rotate around
+    public GameObject curentGrid;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -30,32 +33,32 @@ public class CameraScript : MonoBehaviour
         if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             // go right
-            transform.Translate(new Vector3(speed,0,0));
+            transform.RotateAround(curentGrid.transform.position, -transform.forward, speed);
         }
         if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.Q))
         {
             // go Left
-            transform.Translate(new Vector3(-speed,0,0));
+            transform.RotateAround(curentGrid.transform.position, transform.forward, speed);
         }
         if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
             // go Back
-            transform.Translate(new Vector3(0,0,-speed));
+            transform.RotateAround(curentGrid.transform.position, Vector3.Cross(transform.forward, transform.up), speed);
         }
         if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Z))
         {
             // go Front
-            transform.Translate(new Vector3(0,0,speed));
+            transform.RotateAround(curentGrid.transform.position, -Vector3.Cross(transform.forward, transform.up), speed);
         }
         if(Input.GetKey(KeyCode.Space))
         {
             // go Up
-            transform.Translate(new Vector3(0,speed,0));
+            transform.RotateAround(curentGrid.transform.position, Vector3.forward, speed);
         }
         if(Input.GetKey(KeyCode.C))
         {
             // go Down
-            transform.Translate(new Vector3(0,-speed,0));
+            transform.RotateAround(curentGrid.transform.position, Vector3.forward, speed);
         }
 
         // rotation
