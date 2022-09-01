8bbc7073bc96d99d67c0623c4aecf7e1b9b9e383
MalCaor <xav.lemen@orange.fr>
2022-09-01 16:14:46 +0200

    add wfc

---
+++ Script/PlanetGeneration/WaveFonctionCollapse.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveFonctionCollapse
+{
+    ///
+    /// Class using the wave fonction collapse algo to get a BytMap output from a BytMap input
+    /// will be used to create the terrain of a face
+    ///
+}
