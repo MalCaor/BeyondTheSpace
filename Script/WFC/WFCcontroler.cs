bf691e5b1118066fd88aa294658e0829901d8da8
MalCaor <xav.lemen@orange.fr>
2022-09-01 21:20:09 +0200

    zero

--- Script/WFC/WFCcontroler.cs
+++ Script/WFC/WFCcontroler.cs
@@ -14,11 +14,15 @@ public class WFCcontroler : MonoBehaviour
     public bool periodic; 
     public int symmetry; 
     public bool ground;
+    public int seed;
+    public int limit;
 
     public void Init()
     {
         //'OverlappingModel.OverlappingModel(Texture2D, int, int, int, bool, bool, int, bool, Model.Heuristic)'
         OverlappingModel omWFC = new OverlappingModel(InputTexture, N, width, height, periodicInput, periodic, symmetry, ground, Model.Heuristic.Entropy);
-
+        //'Model.Run(int, int)'
+        omWFC.Run(seed, limit);
+        omWFC.Save(OutputTexture);
     }
 }
