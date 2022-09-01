a7ed3fe6954c8322f8f0c1733a89bd041d5431a9
MalCaor <xav.lemen@orange.fr>
2022-09-01 18:28:13 +0200

    vraiment pas sur

--- Script/WFC/WFCcontroler.cs
+++ Script/WFC/WFCcontroler.cs
@@ -6,10 +6,12 @@ public class WFCcontroler : MonoBehaviour
 {
     WaveFonctionCollapse WFC = new WaveFonctionCollapse();
     public Texture2D bitMap;
+    public Texture2D bitMapOutPut;
+    public int N;
 
     public void GenerateBitMapWFC()
     {
-        Model model;
+        OverlappingModel model = new OverlappingModel(bitMap, N, bitMap.width, bitMap.height, false, false, 0, false, Model.Heuristic.Entropy);
         int height = bitMap.height;
         int width = bitMap.width;
     }
