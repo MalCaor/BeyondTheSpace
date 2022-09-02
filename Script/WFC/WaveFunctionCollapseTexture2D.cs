1ac79564f38ea1689aecae89a9a10f6c8ac38823
MalCaor <xav.lemen@orange.fr>
2022-09-02 19:05:16 +0200

    ReadText

--- Script/WFC/WaveFunctionCollapseTexture2D.cs
+++ Script/WFC/WaveFunctionCollapseTexture2D.cs
@@ -4,5 +4,36 @@ using UnityEngine;
 
 public class WaveFunctionCollapseTexture2D
 {
-    
+    // vars
+    // all color in the og img
+    List<Color> listAllColor;
+    // all proxy of the colors
+    Dictionary<Color, List<Dictionary<Color,int>>> linkNumProxyColbyCol;
+
+    public void run(Texture2D InputTexture)
+    {
+        // init
+        listAllColor = new List<Color>();
+        linkNumProxyColbyCol = new Dictionary<Color, List<Dictionary<Color,int>>>();
+        int height = InputTexture.height;
+        int width = InputTexture.width;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Color curPixel = InputTexture.GetPixel(x, y);
+                // add to simple list
+                if(!listAllColor.Contains(curPixel))
+                {
+                    listAllColor.Add(curPixel);
+                }
+                // add to dic
+                if(!linkNumProxyColbyCol.ContainsKey(curPixel))
+                {
+                    linkNumProxyColbyCol.Add(curPixel, new List<Dictionary<Color,int>>());
+                }
+            }
+        }
+    }
 }
