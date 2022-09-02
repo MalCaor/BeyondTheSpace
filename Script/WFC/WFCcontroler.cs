4a3c38545753bc8de42db32649676d493a66d14c
MalCaor <xav.lemen@orange.fr>
2022-09-02 18:35:07 +0200

    start again WFC but this time on my own

--- Script/WFC/WFCcontroler.cs
+++ Script/WFC/WFCcontroler.cs
@@ -7,22 +7,9 @@ public class WFCcontroler : MonoBehaviour
     // vars
     public Texture2D InputTexture;
     public Texture2D OutputTexture;
-    public int N;
-    public int width;
-    public int height;
-    public bool periodicInput;
-    public bool periodic; 
-    public int symmetry; 
-    public bool ground;
-    public int seed;
-    public int limit;
 
     public void Init()
     {
-        //'OverlappingModel.OverlappingModel(Texture2D, int, int, int, bool, bool, int, bool, Model.Heuristic)'
-        OverlappingModel omWFC = new OverlappingModel(InputTexture, N, width, height, periodicInput, periodic, symmetry, ground, Model.Heuristic.Entropy);
-        //'Model.Run(int, int)'
-        omWFC.Run(seed, limit);
-        omWFC.Save(OutputTexture);
+        WaveFunctionCollapseTexture2D WFC = new WaveFunctionCollapseTexture2D();
     }
 }
