dd3b18edbf96fa925a7d2ff3abe8c9a4d59a81cb
malcaor <xav.lemen@orange.fr>
2022-10-07 21:45:36 +0200

    Add biom class

---
+++ Script/Biom/Biom.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Biom
+{
+    // Handle the Generation of a Grid Collumn during the planet generation
+}
