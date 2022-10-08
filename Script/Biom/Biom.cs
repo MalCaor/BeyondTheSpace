579be7c0d05cf028927b34bc6ef7aba0f2b9eab2
malcaor <xav.lemen@orange.fr>
2022-10-08 14:37:28 +0200

    biom class

--- Script/Biom/Biom.cs
+++ Script/Biom/Biom.cs
@@ -2,7 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Biom
+/// <Summary> 
+/// Handle the Generation of a Grid Collumn during the planet generation.<br/>
+/// Is represented by a Color Pixel.<br/>
+/// Black Pixel is the Default Error Biom.<br/>
+/// </Summary>
+public abstract class Biom
 {
-    // Handle the Generation of a Grid Collumn during the planet generation
+    /// <summary> Color unique to this Biom </summary>
+    Color colorID;
+    /// <summary> Name of the Biom </summary>
+    string nameBiom;
+    
+    /// <summary> 
+    /// Create a Biom with a Color ID
+    /// </summary>
+    /// <param name="color">Color ID of the Biom.</param>
+    /// <param name="name">Name of the Biom.</param>
+    public Biom(Color color, string name)
+    {
+        // set param
+        this.colorID = color;
+        this.nameBiom = name;
+    }
 }
