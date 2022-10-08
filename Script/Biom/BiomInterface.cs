15176c24b94432afe276fcd59ed7da186a6c5c07
malcaor <xav.lemen@orange.fr>
2022-10-08 15:01:08 +0200

    biomInterface

---
+++ Script/Biom/BiomInterface.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <Summary> 
+/// List of Function a Biom must have.<br/>
+/// </Summary>
+public interface BiomInterface
+{
+    /// <Summary> 
+    /// Get all Environements for a tile.<br/>
+    /// </Summary>
+    /// <param name="gridTile">the gridTile.</param>
+    /// <param name="terrainSetting">the planet terrain settings.</param>
+    public List<Environment> GetEnvironments(GridTile gridTile, TerrainPlanetGenerationSetting terrainSetting);
+    /// <Summary> 
+    /// Get all Buildings for a tile.<br/>
+    /// </Summary>
+    /// <param name="gridTile">the gridTile.</param>
+    /// <param name="terrainSetting">the planet terrain settings.</param>
+    public List<Building> GetBuildings(GridTile gridTile, TerrainPlanetGenerationSetting terrainSetting);
+}
