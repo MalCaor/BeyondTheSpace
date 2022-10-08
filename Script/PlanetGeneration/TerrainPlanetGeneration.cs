2b4391994120423bd544601d7d1450afaf42144b
malcaor <xav.lemen@orange.fr>
2022-10-08 15:20:44 +0200

    relly delete usless func

--- Script/PlanetGeneration/TerrainPlanetGeneration.cs
+++ Script/PlanetGeneration/TerrainPlanetGeneration.cs
@@ -72,85 +72,8 @@ public class TerrainPlanetGeneration : MonoBehaviour
         }
     }
 
-    /* OLD CODE ONLY KEEP AS BACK UP
-    void DrawGameObject()
-    {
-        // Nord
-        foreach (GridTile tile in grid.FaceNord)
-        {
-            DrawGameObjectTerrain(tile);
-        }
-        // Est
-        foreach (GridTile tile in grid.FaceEst)
-        {
-            DrawGameObjectTerrain(tile);
-        }
-        // Ouest
-        foreach (GridTile tile in grid.FaceOuest)
-        {
-            DrawGameObjectTerrain(tile);
-        }
-        // Front
-        foreach (GridTile tile in grid.FaceFront)
-        {
-            DrawGameObjectTerrain(tile);
-        }
-        // Back
-        foreach (GridTile tile in grid.FaceBack)
-        {
-            DrawGameObjectTerrain(tile);
-        }
-        // Sud
-        foreach (GridTile tile in grid.FaceSud)
-        {
-            DrawGameObjectTerrain(tile);
-        }
-    }*/
-
     void InitTileTerrain(GridTile t)
     {
-        /// OLD CODE ONLY KEEP AS BACK UP
-        // generate terrain
-        // get angle from planet
-        // pointDownNO is just to have a point to focus on
-        /*
-        float angleUp = Vector3.Angle(planet.transform.up, t.pointDownNO - planet.transform.position);
-        float angleRight = Vector3.Angle(planet.transform.right, t.pointDownNO - planet.transform.position);
-        // evaluate
-        float noiseVal = (noise.Evaluate(new Vector4(angleUp, t.Npos, angleRight, t.Opos) / terrainSetting.roughnessNoiseTerrainElevation)+1) * 0.5f;
-        int levelTerrain = (int)Mathf.Round(gridGener.planetSettings.height * noiseVal);
-        if(t.Dpos>levelTerrain)
-        {
-            t.gridTileManager.tileTerrainType = 0;
-            t.gridTileManager.environmentManager.listEnvironment.Add(Environment.listEnvironmentGlobal.Find((x) => x.name=="air"));
-        } else {
-            t.InitTileGridGameObject();
-            t.gridTileManager.tileTerrainType = 1;
-            t.gridTileManager.environmentManager.listEnvironment.Add(Environment.listEnvironmentGlobal.Find((x) => x.name=="earth"));;
-        }
-        */
-
         /// TODO : GENERATE Terrain per grid from Biom Pixel
     }
-
-    /*    OLD CODE ONLY KEEP AS BACK UP
-    void InitTileWater(GridTile t)
-    {
-        if(t.Dpos<=terrainSetting.waterLevel && t.gridTileManager.tileTerrainType == 0)
-        {
-            t.InitTileGridGameObject();
-            t.gridTileManager.tileTerrainType = 2;
-            t.gridTileManager.environmentManager.listEnvironment.Add(Environment.listEnvironmentGlobal.Find((x) => x.name=="water"));
-        }
-    }*/
-
-    /* OLD CODE ONLY KEEP AS BACK UP
-    void DrawGameObjectTerrain(GridTile t)
-    {
-        if(t.tileGameObject!=null)
-        {
-            t.tileGameObject.GetComponent<GridTileGameObject>().DrawGameObject();
-        }
-    }
-    */
 }
